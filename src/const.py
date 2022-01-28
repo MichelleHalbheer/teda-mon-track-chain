59ea2b10c40b09f13e155fc75cbcf322238c527c
Halbheer Mike <m.halbheer@terradata.ch>
2022-01-28 15:08:06 +0100

    Working Monte Carlo simulation

--- src/const.py
+++ src/const.py
@@ -1,12 +1,32 @@
 # Define constants
 
 import numpy as np
+import os
+
+### ------------------------------ ###
+### -------- DIRECTORIES --------- ###
+### ------------------------------ ###
+
+dir_src = os.path.dirname(__file__)
+dir_root = os.path.dirname(dir_src)
+dir_data = os.path.join(dir_root, 'data')
+
+
+### ------------------------------ ###
+### ----------- VALUES ----------- ###
+### ------------------------------ ###
 
 rho_gon = np.pi / 200 # Conversion factor from gon to radians
 rho_deg = np.pi / 180 # Conversion factor from degree to radians
 
 sigma_dist = 50 # [mm]
 sigma_tilt = 0.072 # [deg]
+sigma_tilt_rad = sigma_tilt * rho_deg
 
 num_sensors = 100 # Number of sensors in track chain
+num_points = num_sensors + 1 # Number of measurement points
 dist = 1.2 # [m] nominal distance between any two sensors
+
+num_reps = int(1e6) # Number of repetitions for Monte Carlo simulation
+
+h0 = 0 # Height level of first point
--- src/main.py
+++ src/main.py
@@ -1,20 +1,41 @@
 # Program to calculate a preadjustment for a traverse to simulate the precision of the TrackChain
 
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 from const import *
 
 def main():
+    monte_carlo()
+
+def monte_carlo():
+    '''
+    Analysis of precision using a Monte Carlo simulation
+    '''
+
+    r_dist = np.random.normal(dist * 1000, sigma_dist, (num_sensors, num_reps))
+    r_ang = np.random.normal(0, sigma_tilt_rad, (num_sensors, num_reps))
+
+    h0 = np.zeros(num_points)
+
+    dh = r_dist * np.sin(r_ang)
+
+    h_free = np.insert(np.cumsum(dh, axis=0), 0 ,np.zeros(num_reps), axis=0)
+
+    h_fixed = h_free - np.multiply(np.repeat(h_free[-1,:][np.newaxis, :], num_points, axis=0), np.arange(0, num_points)[:,np.newaxis] / (num_points - 1))
+
+    fig = plt.figure('Standardabweichung im Verlauf des Zuges')
+    plt.plot(np.arange(101), np.std(h_free, axis=1), label='Einseitig angeschlossen', marker='o',markevery=5)
+    plt.plot(np.arange(101), np.std(h_fixed, axis=1), label='Beidseitig angeschlossen', marker='o',markevery=5)
+    plt.xlabel('Anzahl Punkte')
+    plt.ylabel('Standardabweichung [mm]')
+    plt.legend()
+    plt.grid()
+    plt.show()
     
-    ### Define starting values for height ###
 
-    height_0 = np.zeros(num_sensors) ## Assumed to be zero for all points, as the tilt of all sensors is set to 0
-
-    
-    # Get pairwose height differences of the two points
-    pairwise_deltas = (height_0 - np.insert(height_0[:-1], 0, 0, axis=0))[1:]
-    distances = np.ones(num_sensors) * dist
-
+def empirical_std_tilt():
+    pass
 
 if __name__ == '__main__':
     main()
